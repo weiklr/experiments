ded61f30e38e8f3784dfa7e4f42003886f03066a
wk <wk.liang.84@gmail.com>
2016-03-13 17:58:31 +0800

    added code to remove from given node, beginning and end

--- Java/Algorithms-Chapter1_3/src/com/company/DoubleNodeList.java
+++ Java/Algorithms-Chapter1_3/src/com/company/DoubleNodeList.java
@@ -194,7 +194,7 @@ public class DoubleNodeList<Item> implements Iterable<Item> {
 
         ddl.removeFromEnd(ddl);
         ddl.removeFromBeginning(ddl);
-
+        //test
         /*
         Custom insert at beginning and at end testing (wihtout using insertbefore/after given node
          */
